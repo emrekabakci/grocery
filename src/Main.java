ca251736831781a451c089897393014cb233165f
emrekabakci <emrekabakci.2@icloud.com>
2023-12-13 21:46:08 +0300

    Comment added.

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        //Variables defined
         int pearW, appleW, tomatoesW, bananaW, eggplantW;
         double  pear = 2.14, apple = 3.67, tomatoes = 1.11, banana = 0.95, eggplant = 5.0, amount;
 
+        //New scanner created.
         Scanner inp = new Scanner(System.in);
 
+        //Data retrieved from user.
         System.out.print("Armut kac kilo ? : ");
         pearW = inp.nextInt();
 
@@ -23,8 +26,10 @@ public class Main {
         System.out.print("Patlican kac kilo ? : ");
         eggplantW = inp.nextInt();
 
+        //Amount variable set equal to body mass index.
         amount = (pearW * pear) + (appleW * apple) + (tomatoesW * tomatoes) + (bananaW * banana) + (eggplantW * eggplant);
 
+        //Result printed.
         System.out.print("Toplam Tutar = " + amount + "TL");
     }
 }
